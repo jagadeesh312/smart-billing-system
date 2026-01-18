678c71bf59b2b889c3c95c911bb26fbc9e850797
Jagadeesh <jagadeeshimportant@gmail.com>
2026-01-18 20:02:15 +0530

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
     // ==================== CONFIGURATION ====================
         const defaultConfig = {
             shop_name: 'Smart Billing System',
-            upi_id: '8903559673@ybl'
+            upi_id: '890xxxx@ybl'
         };
 
         let config = { ...defaultConfig };
@@ -1326,3 +1326,4 @@ document.getElementById('paymentModal').classList.add('active');
         init();
     </script>
  <script>(function(){function c(){var b=a.contentDocument||a.contentWindow.document;if(b){var d=b.createElement('script');d.innerHTML="window.__CF$cv$params={r:'9beec561f31bb2bb',t:'MTc2ODU3ODY1Mi4wMDAwMDA='};var a=document.createElement('script');a.nonce='';a.src='/cdn-cgi/challenge-platform/scripts/jsd/main.js';document.getElementsByTagName('head')[0].appendChild(a);";b.getElementsByTagName('head')[0].appendChild(d)}}if(document.body){var a=document.createElement('iframe');a.height=1;a.width=1;a.style.position='absolute';a.style.top=0;a.style.left=0;a.style.border='none';a.style.visibility='hidden';document.body.appendChild(a);if('loading'!==document.readyState)c();else if(window.addEventListener)document.addEventListener('DOMContentLoaded',c);else{var e=document.onreadystatechange||function(){};document.onreadystatechange=function(b){e(b);'loading'!==document.readyState&&(document.onreadystatechange=e,c())}}}})();</script></body>
+
